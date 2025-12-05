8bf9d4b5dc367dc73217d9263305c88b6567af76
Диван Диваныч <ipoltaraus@gmail.com>
2025-12-05 17:33:59 +0300

    Update 25.2

--- main.py
+++ main.py
@@ -27,11 +27,10 @@ def show_budget_details(first_budget, budget, additional_budget, expenses):
     print(f"Начальный бюджет: {first_budget}")
 
 
-def save_budget_details(initial_budget, expenses, first_budget, additional_budget, filepath):
+def save_budget_details(initial_budget, expenses, first_budget, filepath):
     data = {
         "initial_budget": initial_budget,
         "first_budget": first_budget,
-        "additional_budget": additional_budget,
         "expenses": expenses
     }
     with open(filepath, "w") as file:
@@ -42,18 +41,12 @@ def load_budget_data(filepath):
     try:
         with open(filepath, "r") as file:
             data = json.load(file)
-            if data["initial_budget"] > 0:
-                show_budget_details(data["first_budget"], data["initial_budget"], 0, data["expenses"])
-                return data["initial_budget"], data["first_budget"],data["additional_budget"], data["expenses"]
-            else:
-                first_budget = float(input("Пожалуйста, введите имеющееся у вас кол-во денег: "))
-                return first_budget, first_budget, data["additional_budget"], data["expenses"]
+            show_budget_details(data["first_budget"], data["initial_budget"], 0, data["expenses"])
+            return data["initial_budget"], data["first_budget"], data["expenses"]
     except FileNotFoundError:
-        first_budget = float(input("Пожалуйста, введите имеющееся у вас кол-во денег: "))
-        return first_budget, first_budget, 0, {}
+        return 0, 0, {}
     except json.JSONDecodeError:
-        first_budget = float(input("Пожалуйста, введите имеющееся у вас кол-во денег: "))
-        return first_budget, first_budget, 0, {}
+        return 0, 0, {}
 
 
 def update_budget(budget):
@@ -66,20 +59,25 @@ def main():
     parser.add_argument("-f", "--filepath", help="Введите путь до файла с данными.", required=False, default="data.json")
     args = parser.parse_args()
     filepath = args.filepath
-    print("Добро пожаловать, здесь вы сможете отслеживать ваши финансы.\n")
-    initial_budget, first_budget, additional_budget, expenses = load_budget_data(filepath)
+    print("Добро пожаловать, здесь вы сможете отслеживать ваши финансы.")
+    initial_budget, first_budget, expenses = load_budget_data(filepath)
+    additional_budget = 0
+    if initial_budget == 0:
+        initial_budget = float(input("\nПожалуйста, введите имеющееся у вас кол-во денег: "))
+        first_budget = initial_budget
     budget = initial_budget
     while True:
         current_choice = input("\nЧто бы вы хотели сделать?\n1. Добаить траты;\n2. Показать кол-во оставшихся денег;\n3. Обновить бюджет;\n4. Выйти\nВаш выбор 1/2/3/4: ")
         if current_choice == "1":
-            budget, expenses = add_expense(budget, expenses, input("\nВведите описание траты: "), float(input("Введите стоимость покупки: ")))
+            expense_description, expense_amount = input("\nВведите описание траты: "), float(input("Введите стоимость покупки: "))
+            budget, expenses = add_expense(budget, expenses, expense_description, expense_amount)
         elif current_choice == "2":
             show_budget_details(first_budget, budget, additional_budget, expenses)
         elif current_choice == "3":
             additional_budget, budget = update_budget(budget)
         elif current_choice == "4":
             print("\nСпасибо, что воспользовались нашим приложением.\nДо свидания!")
-            save_budget_details(budget, expenses, first_budget, additional_budget, filepath)
+            save_budget_details(budget, expenses, first_budget, filepath)
             break
 
 
